8a084efd17ccd620633653a69ebbb0eed2fe16da
Sam <samuelrystone@gmail.com>
2024-04-10 23:10:21 -0400

    use tailwind config custom colors to refer to var: as it was meant to be! this is so easy

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,21 @@ module.exports = {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  theme: {
+    extend: {
+      colors: {
+        "neutral-one": "var(--neutral-one)",
+        "neutral-two": "var(--neutral-two)",
+        "neutral-three": "var(--neutral-three)",
+        "neutral-inverse-one": "var(--neutral-inverse-one)",
+        "neutral-inverse-two": "var(--neutral-inverse-two)",
+        "neutral-inverse-three": "var(--neutral-inverse-three)",
+        "primary-one": "var(--primary-one)",
+        "primary-two": "var(--primary-two)",
+        "primary-three": "var(--primary-three)",
+      }
+    }
+  },
   plugins: [],
 }
 
