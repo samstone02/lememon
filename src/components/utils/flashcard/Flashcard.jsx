1e9184fb45f7774345e3ded6c661d4286e5259df
Sam <samuelrystone@gmail.com>
2024-04-11 23:50:04 -0400

    flashcard prelim work

---
+++ src/components/utils/flashcard/Flashcard.jsx
@@ -0,0 +1,7 @@
+/**
+ * @summary A flashcard for the user to self-test. Adds functionality on top of a &lt;Flip&gt;
+ * @param {*} props
+ */
+export default function Flashcard(props) {
+	return <div></div>;
+}
---
+++ src/components/utils/flashcard/FlashcardSet.jsx
@@ -0,0 +1,7 @@
+/**
+ * @summary A set of flashcards and wrapping functionality
+ * @param {*} props
+ */
+export default function FlashcardSet(props) {
+	return <div></div>;
+}
